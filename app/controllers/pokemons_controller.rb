de44778447b7332e9d86631be274ab7ad6557d31
titien orefice <titien.orefice@gmail.com>
2024-06-04 17:14:55 +0200

    DBv2

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -8,27 +8,30 @@ class PokemonsController < ApplicationController
     @user = current_user
     @collection = Collection.find(params[:collection_id])
     @album = Album.find(params[:album_id])
+    @seasons = Season.all
+
     if params[:name].present?
-      @pokemons = Pokemon.where("pokemon_name ILIKE ?", "%#{params[:name]}%")
+      @pokemons = Pokemon.where("pokemon_name ILIKE ? OR pokemon_id ILIKE ?", "%#{params[:name]}%", "%#{params[:name]}%")
     else
       @pokemons = []
     end
+
   end
 
 
   # Crée un nouveau Pokémon dans l'album spécifié
-def create
-  @user = current_user
-  @collection = Collection.find(params[:collection_id])
-  @album = Album.find(params[:album_id])
-  @pokemon = Pokemon.find(params[:pokemon_id])
-  @album_pokemon = AlbumPokemon.new(album: @album, pokemon: @pokemon)
-  if @album_pokemon.save
-    redirect_to user_collection_album_path(@user, @collection, @album), notice: 'Pokemon ajouté avec succès.'
-  else
-    render :new
+  def create
+    @user = current_user
+    @collection = Collection.find(params[:collection_id])
+    @album = Album.find(params[:album_id])
+    @pokemon = Pokemon.find(params[:pokemon_id])
+    @album_pokemon = AlbumPokemon.new(album: @album, pokemon: @pokemon)
+    if @album_pokemon.save
+      redirect_to user_collection_album_path(@user, @collection, @album), notice: 'Pokemon ajouté avec succès.'
+    else
+      render :new
+    end
   end
-end
 
   private
 
