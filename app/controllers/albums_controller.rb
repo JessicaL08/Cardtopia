45cf9c1804e1cc8e4442f39bc2d1cc189453e37f
stephane <stephane@wivzem.fr>
2024-06-07 08:21:39 +0200

    add the function to add to album on click

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -64,7 +64,7 @@ end
 
   def search_pokemon
 
-    
+
     if params[:type].present? && params[:extension_id].present?
       @pokemons = @pokemons.where("extension_id = ?", params[:extension_id])
       @pokemons = @pokemons.where("metadata @> ?", { types: [I18n.t("pokemon_types.#{params[:type]}")] }.to_json)
