b40264b6f7c5d9f5812bb013d160da27ad549583
stephane <stephane@wivzem.fr>
2024-06-07 23:40:35 +0200

    sidebar making and useless button removing

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,3 +3,6 @@
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
  //= link info_button.js
+ //= link sticky_button.js
+ //= link side_bar_actions.js
+ //= link activate_button_delete.js
