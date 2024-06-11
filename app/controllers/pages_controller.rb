8359bde53398ac6a70443660c3e2635d90904efe
stephane <stephane@wivzem.fr>
2024-06-11 08:49:52 +0200

    Add logic when a signed user is redirected to his album page instead of anything else when clicking on logo or home button

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < ApplicationController
   def home
-    @collections = current_user.collections if user_signed_in?
+    @user_collections = current_user.collections
+
+    if @user_collections.present? && @user_collections.first.albums.any?
+      redirect_to collection_path(@user_collections.first)
+    else
+      @collections = current_user.collections if user_signed_in?
+    end
   end
 end
