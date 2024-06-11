4027b94411405370300b628cebc29f1d8bad141d
titien orefice <titien.orefice@gmail.com>
2024-06-11 09:24:40 +0200

    debut code carousel

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -3,5 +3,6 @@ class PokemonsController < ApplicationController
   def show
     # @pokemon = Pokemon.includes(name:).find(params[:album_pokemon_id])
     @pokemon = Pokemon.find(params[:album_pokemon_id])
+    @album = Album.find(params[:album_id])
   end
 end
