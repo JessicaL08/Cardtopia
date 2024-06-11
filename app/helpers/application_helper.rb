4d501068df3ac2d8f482a0e6118636e59f311f21
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2024-06-11 15:33:07 +0200

    finished scroll card

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-  def pokemon_image_tag(pokemon)
+  def pokemon_image_tag(pokemon, index = nil)
     if pokemon.image != "/high.webp"
-      image_tag(pokemon.image, class: "mandatory-card pokemon-image-info")
+      image_tag(pokemon.image, class: "mandatory-card pokemon-image-info", "data-index": index, id: "#{pokemon.id}")
     else
-      image_tag("Pika-no-image", class: "mandatory-card pokemon-image-info no-image")
+      image_tag("Pika-no-image", class: "mandatory-card pokemon-image-info no-image", "data-index": index, id: "#{pokemon.id}")
     end
   end
 end
