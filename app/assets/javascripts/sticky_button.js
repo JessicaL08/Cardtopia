9f0d8f234ba4bf7861691935d787f6c0b8e51265
jessica lemaire <lemaire.jessica@gmail.com>
2024-06-11 04:44:19 +0200

    proposition bouton nav mobile like

--- app/assets/javascripts/sticky_button.js
+++ app/assets/javascripts/sticky_button.js
@@ -1,17 +1,21 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const stickyButton = document.getElementById("stickyButton");
-    const subButtons = document.getElementById("subButtons");
-            console.log("j'écoute la page");
+document.addEventListener("DOMContentLoaded", function () {
+  const stickyButton = document.getElementById("stickyButton");
+  const subButtons = document.getElementById("subButtons");
+  console.log("j'écoute la page");
 
-    stickyButton.addEventListener("click", function(event) {
-        console.log("premier event");
-        // event.stopPropagation(); // Pas de propagation du clic
-        subButtons.style.display = subButtons.style.display === "none" ? "block" : "none";
-    });
+  stickyButton.addEventListener("click", function (event) {
+    console.log("premier event");
+    // event.stopPropagation(); // Pas de propagation du clic
+    subButtons.style.visibility =
+      subButtons.style.visibility === "hidden" ? "visible" : "hidden";
+  });
 
-    document.addEventListener("click", function(event) {
-        if (!stickyButton.contains(event.target) && !subButtons.contains(event.target)) {
-            subButtons.style.display = "none";
-        }
-    });
+  document.addEventListener("click", function (event) {
+    if (
+      !stickyButton.contains(event.target) &&
+      !subButtons.contains(event.target)
+    ) {
+      subButtons.style.visibility = "hidden";
+    }
+  });
 });
