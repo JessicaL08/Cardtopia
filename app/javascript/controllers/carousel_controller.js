94a339109ff9816f5484828ab776eb19f3a855de
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2024-06-11 14:55:22 +0200

    scroll cards

--- app/javascript/controllers/carousel_controller.js
+++ app/javascript/controllers/carousel_controller.js
@@ -1,6 +1,17 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   connect() {
+    console.log("merci");
+  }
+
+  scroll(event) {
+    console.log("scrolling");
+    console.log(event);
+  }
+
+  end(event) {
+    console.log("end");
+    console.log(event);
   }
 }
