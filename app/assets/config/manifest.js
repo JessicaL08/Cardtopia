ad2c7605374b895eca08f46441123dcc4c1aedbd
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2024-06-13 10:40:33 +0200

    manifest.json.erb

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,12 +2,14 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
- //= link info_button.js
- //= link sticky_button.js
- //= link side_bar_actions.js
- //= link activate_button_delete.js
- //= link side_bar_button_home.js
- //= link search_script.js
+//= link info_button.js
+//= link sticky_button.js
+//= link side_bar_actions.js
+//= link activate_button_delete.js
+//= link side_bar_button_home.js
+//= link search_script.js
 //= link side_bar_button_show_collection.js
 //= link faq_animation.js
 //= link side_bar_button_new.js
+
+//= link manifest.json
