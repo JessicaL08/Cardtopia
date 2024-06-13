9f19bb47dd68bfa95fd2d6bb944fc923a91ea52c
stephane <stephane@wivzem.fr>
2024-06-13 10:27:12 +0200

    tests slide TA

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,5 +1,6 @@
 class PokemonsController < ApplicationController
   def show
+
     # @pokemon = Pokemon.includes(name:).find(params[:album_pokemon_id])
     @pokemon = Pokemon.find(params[:album_pokemon_id])
     @album = Album.find(params[:album_id])
@@ -8,11 +9,15 @@ class PokemonsController < ApplicationController
     first_part = pokemons.slice(0, pokemons.index(@pokemon))
     second_part = pokemons.slice(pokemons.index(@pokemon), pokemons.count)
     @reordered_pokemons = second_part.concat(first_part)
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @pokemon }
+    end
   end
 
   def render_content
     @pokemon = Pokemon.find(params[:id])
-
     render json: {
       partial: render_to_string(
         partial: "pokemons/pokemon_content",
